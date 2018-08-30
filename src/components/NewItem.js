e040ed9749c27b38ee571506f935f2e270df431d
psx980626 <375180728@qq.com>
2018-08-30 18:58:42 +0800

    ...

--- src/components/NewItem.js
+++ src/components/NewItem.js
@@ -18,7 +18,7 @@ class NewItem extends Component {
     }
 
     timestampToTime(timestamp){
-        var date = new Date(timestamp * 1000);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+        var date = new Date(timestamp);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
         var Y = date.getFullYear() + '-';
         var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1);
         if(parseInt(M) < 10 ){
@@ -28,7 +28,7 @@ class NewItem extends Component {
         var h = date.getHours() + ':';
         var m = date.getMinutes() + ':';
         var s = date.getSeconds();
-        return M +'-'+D+h+m+s;
+        return M +'-'+D+h+m;
     }
 
     render() {
