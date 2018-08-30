d35d9ce824798b876bacdb01b1c108a602b35bb5
psx980626 <375180728@qq.com>
2018-08-30 18:47:07 +0800

    ...

--- src/components/NewItem.js
+++ src/components/NewItem.js
@@ -17,6 +17,20 @@ class NewItem extends Component {
         this.match = this.props.match;
     }
 
+    timestampToTime(timestamp){
+        var date = new Date(timestamp * 1000);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+        var Y = date.getFullYear() + '-';
+        var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1);
+        if(parseInt(M) < 10 ){
+            M = M.substring(1,2)
+        }
+        var D = date.getDate() + ' ';
+        var h = date.getHours() + ':';
+        var m = date.getMinutes() + ':';
+        var s = date.getSeconds();
+        return M +'-'+D+h+m+s;
+    }
+
     render() {
         const obj = this.props.obj;
         console.log(obj);
@@ -27,7 +41,7 @@ class NewItem extends Component {
             window.loaction = './takePic';
             return;
         }
-        const time = obj.date.slice(5, 10);
+        const time = this.timestampToTime(obj.date);
         const match = this.match;
         return (
             <div className="showListItem" key={rowID}>
