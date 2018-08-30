46ad9efd7c0a5a6ea0c0243d6a41f08673daf372
psx980626 <375180728@qq.com>
2018-08-31 00:09:06 +0800

    ...

--- src/components/ClassList.js
+++ src/components/ClassList.js
@@ -62,14 +62,14 @@ class ClassList extends Component {
         });
         console.log(this.state.data)
 
-        const hei = document.documentElement.clientHeight - ReactDOM.findDOMNode(this.lv).parentNode.offsetTop;
+        // const hei = document.documentElement.clientHeight - ReactDOM.findDOMNode(this.lv).parentNode.offsetTop;
         // simulate initial Ajax
         setTimeout(() => {
             this.genData();
             this.setState({
                 dataSource: this.state.dataSource.cloneWithRowsAndSections(this.dataBlobs, this.sectionIDs, this.rowIDs),
                 isLoading: false,
-                height: hei,
+                height: 1200,
             });
         }, 600);
     }
--- src/components/NewList.js
+++ src/components/NewList.js
@@ -64,14 +64,14 @@ class NewList extends Component {
         });
         this.data = this.state.data;
 
-        // const hei = document.documentElement.clientHeight - ReactDOM.findDOMNode(this.lv).parentNode.offsetTop;
+        const hei = document.documentElement.clientHeight - ReactDOM.findDOMNode(this.lv).parentNode.offsetTop;
         // simulate initial Ajax
         setTimeout(() => {
             this.genData();
             this.setState({
                 dataSource: this.state.dataSource.cloneWithRowsAndSections(this.dataBlobs, this.sectionIDs, this.rowIDs),
                 isLoading: false,
-                height: 400,
+                height: hei,
             });
         }, 600);
     }
